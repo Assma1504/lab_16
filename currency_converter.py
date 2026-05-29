a5bb25d6433eeb09252222b44e87013f586c9827
Assma1504 <assmabaziz15@gamil.com>
2026-05-29 22:37:29 +0300

    task completed

--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,5 @@
 
+import requests
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
@@ -37,7 +38,7 @@ class Ui_MainWindow(object):
         self.from_value.setFont(font)
         self.from_value.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.from_value.setObjectName("from_value")
-        self.from_value.setPlaceholderText("From")
+        self.from_value.setPlaceholderText("0")
         self.to_value = QtWidgets.QLineEdit(self.frameTop)
         self.to_value.setGeometry(QtCore.QRect(160, 210, 171, 40))
         font = QtGui.QFont()
@@ -48,9 +49,9 @@ class Ui_MainWindow(object):
         self.to_value.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.to_value.setObjectName("to_value")
         self.to_value.setEnabled(False)
-        self.to_value.setPlaceholderText("To")
-        self.convert_btn = QtWidgets.QPushButton(self.frameTop, clicked = lambda : self.press_btn("7"))
-        self.convert_btn.setGeometry(QtCore.QRect(10, 145, 100, 50))
+        self.to_value.setPlaceholderText("0")
+        self.convert_btn = QtWidgets.QPushButton(self.frameTop, clicked = self.get_data)
+        self.convert_btn.setGeometry(QtCore.QRect(10, 145, 100, 55))
         self.convert_btn.setText("")
         self.convert_btn.setObjectName("convert_btn")
         self.frameButtom = QtWidgets.QFrame(self.centralwidget)
@@ -94,7 +95,7 @@ class Ui_MainWindow(object):
         font.setWeight(50)
         self.button_one.setFont(font)
         self.button_one.setObjectName("button_one")
-        self.button_c = QtWidgets.QPushButton(self.frameButtom, clicked = lambda : self.press_btn("C"))
+        self.button_c = QtWidgets.QPushButton(self.frameButtom, clicked = self.clear_input)
         self.button_c.setGeometry(QtCore.QRect(10, 220, 100, 60))
         font = QtGui.QFont()
         font.setPointSize(26)
@@ -162,9 +163,53 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+#============================================================= Set values to QComboBox ==========================================================
+        currency_list = [
+    "USD", "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", 
+    "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", 
+    "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLF", 
+    "CLP", "CNH", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", 
+    "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", 
+    "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", 
+    "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", 
+    "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", 
+    "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", 
+    "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", 
+    "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", 
+    "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", 
+    "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", 
+    "SLL", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", 
+    "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "UYU", 
+    "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XCG", "XDR", "XOF", 
+    "XPF", "YER", "ZAR", "ZMW", "ZWG", "ZWL"
+]
+        
+        self.from_currency.addItems(currency_list)
+        self.to_currency.addItems(currency_list)
+
+#============================================================= Methods ==========================================================
 
     def press_btn(self, pressedBtn):
-        print(pressedBtn)
+        self.from_value.setText(f"{self.from_value.text()}{pressedBtn}")
+
+    def clear_input(self):
+        self.from_value.clear()
+        self.to_value.clear()
+
+    def get_data(self):
+
+            api_key="44a0c92786b03817601ea22c"
+            selected_currency_from = self.from_currency.currentText()
+            amount =float(self.from_value.text())
+            selected_currency_to = self.to_currency.currentText()
+
+            
+            url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{selected_currency_from}"
+            response = requests.get(url)
+            data = response.json()
+            cource = float(data['conversion_rates'][selected_currency_to])
+            result = amount * cource
+            self.to_value.setText(str(result))
 
 
     def retranslateUi(self, MainWindow):
